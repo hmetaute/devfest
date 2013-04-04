ae03ec2754a7efeeecfcc29eb2fa920b05edc1f0
Hernan Dario Metaute Sarmiento <hernan.metaute@ceiba.com.co>
2013-04-03 22:58:33 -0500

    se borran los de la app principal

--- app_inventario/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AppInventario::Application.config.secret_token = '68fcb174887d15711d7f5e39eea19174eaeeff23905b21ede689fd4c91cd3c197417ccbf1a5328ebf12bc121e80bdce347fcaca39d77e0b89642405fa0c12706'
